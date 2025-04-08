0a1fd004272f00cd010675fa889d92540b9b7e2f
Inder Grewal <inderdeepg8@gmail.com>
2025-04-08 14:47:24 -0700

    added function to calculate operations

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-var display = '0';
+var display = '';
 var num1 = null;
 var num2 = null;
 var operator = null;
@@ -15,10 +15,23 @@ buttons.forEach((button) => {
 
 function updateTextField(value) {
     const textField = document.querySelector('#textField');
-    display = display + value;
-    if (display.length > 19) {
-        display = display.substring(0, 19);
+
+    if (value === 'sign') {
+        console.log(display.charAt(0));
+        if (display.charAt(0) === '-') {
+            display = display.slice(1);
+        } else {
+            display = '-' + display;
+        }
+        console.log(display.charAt(0));
+    } else {
+        display = display + value;
+        if (display.length > 19) {
+            display = display.substring(0, 19);
+        }
+
     }
+
     const data = textField.textContent = display;
     return data;
 }
@@ -33,48 +46,73 @@ function operate(clickedButton) {
     }
 
     if (className === 'operator') { //operator
-        if (answer != null)
-        if (operator === null && num2 === null) {
-            clickedButton.style.background = 'blue';
-            operator = '*';
+        if (answer != null) {
+            num1 = answer;
+            answer = null;
         }
+        if (operator === null && num2 === null) {
+            clickedButton.style.background = '#ffa600a6';
+            operator = clickedButton.id;
+        } else if (operator != null && num2 === null) {
+            num2 = num1;
+            //run className == 'other' by passing 'equals'
+        }
+
     } else if(className === 'other') { //input is not a operator (AC, +/-, )
+        if (operator != null) document.getElementById(`${operator}`).style.background = '#ffa600';  //Breaks after pressing = since operator is null (either put in if statement of (better:) disable = until operator isn't null)
         if (clickedButton.id === 'clear') {
-            num1 = null;
-            num2 = null;
-            operator = null;
             answer = null;
             display = '';
-            updateTextField('0');
+            updateTextField('0');   //
 
+        } else if (clickedButton.id === 'equals') {
+            display = '';
+            calculate();
+            updateTextField(answer);
+        } else if (clickedButton.id === 'sign') {
+            updateTextField('sign');
         }
+
+        num1 = null;
+        num2 = null;
+        operator = null;
+
     } else if (className === 'operand'){ //input is a number (operand)
         if (operator === null && num2 === null) {
+            if (answer != null && num1 === null) {
+                display = ' ';
+            }
+            answer = null;
             num1 = updateTextField(clickedButton.value);
         } else {
             num2 = updateTextField(clickedButton.value);
         }
     }
 
-    console.log(num1 + ' ' + operator + ' ' + num2);
+    console.log(num1 + ' ' + operator + ' ' + num2 + ' ' + answer);
 }
 
-function multiply() {
-    answer = num1 * num2;
-}
+function calculate() {
+    const num1_Cal = Number(num1);
+    const num2_Cal = Number(num2);
 
-function division() {
-    answer = num1 / num2;
-}
-
-function addition() {
-    answer = num1 + num2;
-}
-
-function subtract() {
-    answer = num1 - num2;
-}
-
-function modulo() {
-    answer = num1 % num2;
+    switch (operator) {
+        case '%':
+            answer = (num1_Cal % num2_Cal);
+            break;
+        case '/':
+            answer = Math.round((num1_Cal / num2_Cal) * 100) / 100;
+            break;
+        case '*':
+            answer = num1_Cal * num2_Cal;
+            break;
+        case '-':
+            answer = num1_Cal - num2_Cal;
+            break;
+        case '+':
+            answer = num1_Cal + num2_Cal;
+            break;
+        default:
+            console.log('error');
+    }
 }
